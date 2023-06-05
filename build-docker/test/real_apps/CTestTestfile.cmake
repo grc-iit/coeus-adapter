5315804387cdd2364018d24ae6e29f8e1e4022a6
jaime <jcernudagarcia@hawk.iit.edu>
2023-06-04 21:32:35 -0500

    Changes to the CMakes to accept hermes and to clean some of the dependency chains

---
+++ build-docker/test/real_apps/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/coeus-adapter/test/real_apps
+# Build directory: /tmp/coeus-adapter/build-docker/test/real_apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gray-scott")
