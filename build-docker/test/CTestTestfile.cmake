f62fed3ec3bbe92ef0e27b19e4f7e958ecab48e3
jaime <jcernudagarcia@hawk.iit.edu>
2023-06-04 21:32:35 -0500

    Changes to the CMakes to accept hermes and to clean some of the dependency chains

---
+++ build-docker/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/coeus-adapter/test
+# Build directory: /tmp/coeus-adapter/build-docker/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("unit")
+subdirs("real_apps")
