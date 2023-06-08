523bfd83ee24eb282a423de535d46906e7628de3
Augusto Diaz-Leante Ferreiro <adiazleanteferreiro@hawk.iit.edu>
2023-06-07 20:00:59 -0500

    Basic.cc update

--- test/unit/basic.cc
+++ test/unit/basic.cc
@@ -3,6 +3,8 @@
 //
 
 #include "coeus/hermes_engine.h"
+namespace hapi = hermes::api;
+
 
 int main() {
   adios2::ADIOS adios;
@@ -35,14 +37,49 @@ int main() {
     "myVar", shape, start, count);
 
   // Write to file
+  hapi::Hermes::HermesEngine writer = EngineCreate(io, file, adios2::Mode::Write ); // Falta un parametro
+  auto bkt1 = writer->GetBucket("myVar"); // esto no se si esta bien asi (si no ponemos "myVar"). El writer seria el objeto hermes
+  size_t blob_size = KILOBYTES(4);
+  hapi::Hermes::api::Context ctx;
+  hapi::Hermes::BlobId blob_id1;
+  hapi::Hermes::Blob blob(blob_size);
+  memset(blob.data(), data.data(), blob_size); // no se como se relaciona con las io de adios
+  bkt1.Put(var, blob, blob_id1, ctx); // no se si pasar la var de adios es valido
+
+  /*
   adios2::Engine writer = io.Open(file, adios2::Mode::Write);
   writer.Put(var, data.data());
-  writer.Close();
+  writer.Close();*/
 
   // Read from file
-  adios2::Engine reader = io.Open(file, adios2::Mode::Read);
-  reader.Get(var, data.data());
-  reader.Close();
+  hapi::Hermes::HermesEngine reader = EngineCreate(io, file, adios2::Mode::Read ); //Falta un parametro que no se que es
+  auto bkt2 = reader->GetBucket(var.name); // esto no se si esta bien asi (si no ponemos "myVar"). El writer seria el objeto hermes
+  hapi::Hermes::api::Context ctx2;
+  hapi::Hermes::Blob blob2;
+  bkt2.GetBlobId(var.name, blob_id);
+  REQUIRE(!blob_id.IsNull());
+  bkt2.Get(blob_id, blob2, ctx2);
+
+    /*
+    adios2::Engine reader = io.Open(file, adios2::Mode::Read);
+    reader.Get(var, data.data());
+    reader.Close();*/
+  using HermesPtr = std::shared_ptr<hapi::Hermes>;
+
+
+  hapi::Bucket bkt("shared bucket", HermesPtr); // create bucket ?withfilename?
+  constexpr int bytes_per_blob = KILOBYTES(3);
+
+  hapi::Blob blobPut(bytes_per_blob, data.data()); //aqui el hace con x, creo que podriamos rellenarlo con la data correspondiente?
+  status = bkt.Put("varName", blobPut);
+  Assert(status.Succeeded());
+
+  hapi::Blob blobGet;
+  size_t blob_size = bucket.Get(blob_name, blobGet);
+
+  Assert(get_result == put_data);
+
+  bkt.Destroy();
 
   std::cout << "Done" << std::endl;
 
