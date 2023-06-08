84fa6a0c32dce2b54bb9f7c80ead1a612d5dbeaa
jaime <jcernudagarcia@hawk.iit.edu>
2023-06-07 19:15:24 -0500

    Removed gray-scott app from the linting

--- CI/lint.sh
+++ CI/lint.sh
@@ -6,4 +6,5 @@ cpplint --recursive \
 --exclude="${PROJECT_ROOT}/build_docker" \
 --exclude="${PROJECT_ROOT}/CMake" \
 --exclude="${PROJECT_ROOT}/CI" \
+--exclude="${PROJECT_ROOT}/test/real_apps/gray-scott" \
 "${PROJECT_ROOT}"
