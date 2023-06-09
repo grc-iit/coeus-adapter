4dc019f2d66b9601e91558ecfe6fc6e32d148db6
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-06-09 09:27:45 -0500

    back to previous test

--- test/unit/basic.cc
+++ test/unit/basic.cc
@@ -45,7 +45,7 @@ int main() {
     "myVar", shape, start, count);
 
   // Write to file
-  coeus::HermesEngine* writer = EngineCreate(io, file, adios2::Mode::Write); // Needs an extra param comm
+  /*coeus::HermesEngine* writer = EngineCreate(io, file, adios2::Mode::Write); // Needs an extra param comm
   //hapi::Hermes::CreateBucket("bkt1");
   auto bkt1 = writer->GetBucket("myVar");
   size_t blob_size = KILOBYTES(4);
@@ -53,36 +53,35 @@ int main() {
   hermes::BlobId blob_id1;
   hermes::Blob blob(blob_size);
   memset(blob.data(), data.data(), blob_size);
-  bkt1.Put(var, blob, blob_id1, ctx);
+  bkt1.Put(var, blob, blob_id1, ctx);*/
 
   //for(size_t i = 0; i < blob_size; ++i){
     //  memset(blob.data(), i, blob_size);
      // bkt1.Put("myVar", blob, blob_id1, ctx);
   //}
 
-/*
+
   // Write to file
   adios2::Engine writer = io.Open(file, adios2::Mode::Write);
   writer.Put(var, data.data());
   writer.Close();
-*/
+
 
   // Read from file
-  coeus::HermesEngine* reader = EngineCreate(io, file, adios2::Mode::Read); // Needs an extra param
+ /* coeus::HermesEngine* reader = EngineCreate(io, file, adios2::Mode::Read); // Needs an extra param
   // hapi::Hermes::HermesEngine reader = EngineCreate(io, file, adios2::Mode::Read); // Needs an extra param
   auto bkt2 = reader->GetBucket("myVar");
   hapi::Context ctx2;
   hermes::Blob blob2;
   bkt2.GetBlob("myVar", blob_id);
   REQUIRE(!blob_id.IsNull());
-  bkt2.Get(blob_id, blob2, ctx2);
+  bkt2.Get(blob_id, blob2, ctx2);*/
 
-  /*
   // Read from file
   adios2::Engine reader = io.Open(file, adios2::Mode::Read);
   reader.Get(var, data.data());
   reader.Close();
-*/
+
   std::cout << "Done" << std::endl;
 
   return 0;
