c9c6719319de24846c689cf2a1ff0adb3d4a8ccc
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-06-10 17:21:29 -0500

    Inital test configuration

---
+++ test/tests.py
@@ -0,0 +1,17 @@
+from py_hermes_ci.test_manager import TestManager
+from jarvis_util import *
+
+class NativeTestManager(TestManager):
+    def spawn_all_nodes(self):
+        return self.spawn_info()
+
+    def set_paths(self):
+        self.BASIC_CMD = f"{self.CMAKE_BINARY_DIR}/bin/basic"
+
+    def basic(self):
+        spawn_info = self.spawn_info(nprocs=1,
+                                     hermes_conf='hermes_server')
+        self.start_daemon(spawn_info)
+        node = Exec(self.TEST_BUFFER_POOL_CMD, spawn_info)
+        self.stop_daemon(spawn_info)
+        return node.exit_code
