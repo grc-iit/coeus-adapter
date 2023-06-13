0ea91cb1d10b9b3d56127c7ed685011066e867d1
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-06-13 15:18:32 -0500

    Script adapted to COEUS

--- CI/py_coeus_ci/bin/run_test.py
+++ CI/py_coeus_ci/bin/run_test.py
@@ -13,27 +13,19 @@ if __name__ == '__main__':
     test_name = sys.argv[2]
     cmake_binary_dir = sys.argv[3]
     address_sanitizer = False
-    # The root of Hermes
-    HERMES_ROOT = str(pathlib.Path(__file__).parent.
-                      parent.parent.parent.resolve())
     # The root of Coeus
-    # COEUS_ROOT = str(pathlib.Path(__file__).parent.
-      #                 parent.parent.parent.resolve())
+    COEUS_ROOT = str(pathlib.Path(__file__).parent.
+                      parent.parent.parent.resolve())
 
-    ADAPTER_TEST_ROOT = f"{HERMES_ROOT}/adapter/test"
-    # ADAPTER_TEST_ROOT = f"{COEUS_ROOT}/adapter/test" # Probably need to add the directory /adapter
+    ADAPTER_TEST_ROOT = f"{COEUS_ROOT}/adapter/test" # Do we need to add the directory?
 
     # Ensure that all calls beneath know how to resolve jarvis_util
-    sys.path.insert(0, f"{HERMES_ROOT}/ci/jarvis-util")
-    # sys.path.insert(0, f"{COEUS_ROOT}/ci/jarvis-util")
+    sys.path.insert(0, f"{COEUS_ROOT}/CI/jarvis-util")
 
-    # Ensure subsequent classes know how to resolve py_hermes_ci package
-    sys.path.insert(0, f"{HERMES_ROOT}/ci/py_hermes_ci")
-    # sys.path.insert(0, f"{COEUS_ROOT}/ci/py_hermes_ci")
+    # Ensure subsequent classes know how to resolve py_coeus_ci package
+    sys.path.insert(0, f"{COEUS_ROOT}/CI/py_coeus_ci")
 
     # Choose which unit test file to (we are just going to test 'Native')
-    # if test_type == 'native':
-    #   pkg_dir = f"{COEUS_ROOT}/test"
     if test_type == 'stdio':
         pkg_dir = f"{ADAPTER_TEST_ROOT}/{test_type}"
     elif test_type == 'posix':
@@ -42,14 +34,14 @@ if __name__ == '__main__':
         pkg_dir = f"{ADAPTER_TEST_ROOT}/{test_type}"
     elif test_type == 'vfd':
         pkg_dir = f"{ADAPTER_TEST_ROOT}/{test_type}"
-    if test_type == 'native':
-        pkg_dir = f"{HERMES_ROOT}/test"
+    elif test_type == 'native':
+        pkg_dir = f"{COEUS_ROOT}/test"
     elif test_type == 'data_stager':
-        pkg_dir = f"{HERMES_ROOT}/data_stager/test"
+        pkg_dir = f"{COEUS_ROOT}/data_stager/test"
     elif test_type == 'kvstore':
-        pkg_dir = f"{HERMES_ROOT}/adapter/kvstore"
+        pkg_dir = f"{COEUS_ROOT}/adapter/kvstore"
     elif test_type == 'java_wrapper':
-        pkg_dir = f"{HERMES_ROOT}/wrapper/java"
+        pkg_dir = f"{COEUS_ROOT}/wrapper/java"
     else:
         raise Exception("Could not find the unit test")
     # Load the unit test
@@ -58,6 +50,5 @@ if __name__ == '__main__':
     test_cls = load_class(f"tests",
                           pkg_dir,
                           to_camel_case(f"{test_type}_test_manager"))
-    tests = test_cls(HERMES_ROOT, cmake_binary_dir, address_sanitizer)
-    # tests = test_cls(COEUS_ROOT, cmake_binary_dir, address_sanitizer)
+    tests = test_cls(COEUS_ROOT, cmake_binary_dir, address_sanitizer)
     tests.call(test_name)
