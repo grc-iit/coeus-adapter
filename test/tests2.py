0bc81a8707da1245e1cd8bfed53eb18212d2c2a1
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-06-23 18:48:01 -0500

    Point gs.bp path

--- test/tests2.py
+++ test/tests2.py
@@ -10,6 +10,7 @@ class NativeTestManager(TestManager):
         self.GraySIM_CMD = f"{self.CMAKE_BINARY_DIR}/bin/adios2-gray-scott"
         self.GrayCalc_CMD = f"{self.CMAKE_BINARY_DIR}/bin/adios2-pdf-calc"
         self.Simulation_Path = f"{self.CMAKE_SOURCE_DIR}/test/real_apps/gray-scott/simulation"
+        self.gsbp_path = f"{self.CMAKE_BINARY_DIR}/test/real_apps/gray-scott"
 
     def test_basic(self):
             spawn_info = self.spawn_info(nprocs=1,
@@ -22,16 +23,16 @@ class NativeTestManager(TestManager):
     def test_gray_scott_sim(self):
         spawn_info = self.spawn_info(nprocs=1,
                                      hermes_conf='hermes_server')
-        self.start_daemon(spawn_info)
-        node = Exec(f"mpirun -n 4 {self.GraySIM_CMD} {self.Simulation_Path}/settings-files.json", spawn_info)
-        self.stop_daemon(spawn_info)
+        #self.start_daemon(spawn_info)
+        node = Exec(f"mpirun -n 1 {self.GraySIM_CMD} {self.Simulation_Path}/settings-files.json", spawn_info)
+        #self.stop_daemon(spawn_info)
         return node.exit_code
 
 
     def test_pdf_calc(self):
         spawn_info = self.spawn_info(nprocs=1,
                                      hermes_conf='hermes_server')
-        self.start_daemon(spawn_info)
-        node = Exec(f"mpirun -n 2 {self.GrayCalc_CMD} {self.Simulation_Path}/gs.bp pdf.bp 100", spawn_info)
-        self.stop_daemon(spawn_info)
+        #self.start_daemon(spawn_info)
+        node = Exec(f"mpirun -n 1 {self.GrayCalc_CMD} {self.gsbp_path}/gs.bp pdf.bp 100", spawn_info)
+        #self.stop_daemon(spawn_info)
         return node.exit_code
