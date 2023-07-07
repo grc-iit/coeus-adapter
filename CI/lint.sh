eb1546321fd9de10ecd88e50625be617f2c54c2c
jaime <jcernudagarcia@hawk.iit.edu>
2023-07-07 15:43:01 -0500

    Updated the excluded path on the lint.sh

--- CI/lint.sh
+++ CI/lint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 PROJECT_ROOT=$1
+
+echo "linting"
 cpplint --recursive \
 --exclude="${PROJECT_ROOT}/build" \
+--exclude="/tmp/coeus-adapter/build" \
+--exclude="${PROJECT_ROOT}/build-ares" \
+--exclude="/tmp/coeus-adapter/build-ares" \
 --exclude="${PROJECT_ROOT}/build_docker" \
+--exclude="/tmp/coeus-adapter/build_docker" \
 --exclude="${PROJECT_ROOT}/CMake" \
 --exclude="${PROJECT_ROOT}/CI" \
 --exclude="${PROJECT_ROOT}/test/real_apps/gray-scott" \
