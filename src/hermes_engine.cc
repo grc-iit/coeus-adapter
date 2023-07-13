40b08faa1b8d996143c5503379df0c574eee754b
jaime <jcernudagarcia@hawk.iit.edu>
2023-07-12 20:29:30 -0500

    Added spdlog to the project

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -11,6 +11,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "coeus/hermes_engine.h"
+#include "spdlog/spdlog.h"
 #include <stdio.h>
 #include <stdlib.h>
 
