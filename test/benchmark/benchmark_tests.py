6708fe43000d09410878b52d8ea15cb1fdfac684
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-07-24 18:04:22 +0200

    Changed nº of processes for testing and file output path

--- test/benchmark/benchmark_tests.py
+++ test/benchmark/benchmark_tests.py
@@ -66,7 +66,7 @@ class NativeTestManager(TestManager):
     def test_gray_scott_analysis_file_parallel_bench(self, num_processes):
         self.prepare_simulation("file")
         spawn_info = self.spawn_info(cwd=self.INSTALL_PATH)
-        simulation = Exec(f"mpirun -n 3 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-gray-scott settings-files.json", spawn_info)
+        simulation = Exec(f"mpirun -n 2 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-gray-scott settings-files.json", spawn_info)
         analysis = Exec(f"mpirun -n 3 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-pdf-calc gs.bp pdf.bp 100", spawn_info)
         Copy(f"gs.bp", f"/mnt/nvme/jmendezbenegassimarq/gs.bp", spawn_info)
         Copy(f"ckpt.bp", f"/mnt/nvme/jmendezbenegassimarq/ckpt.bp", spawn_info)
