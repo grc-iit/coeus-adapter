3047594a5873ae4a7ba40fb3366144b0ab2d749a
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-07-25 11:55:47 +0200

    Changed parameters for testing

--- test/benchmark/benchmark_tests.py
+++ test/benchmark/benchmark_tests.py
@@ -66,7 +66,7 @@ class NativeTestManager(TestManager):
     def test_gray_scott_analysis_file_parallel_bench(self, num_processes):
         self.prepare_simulation("file")
         spawn_info = self.spawn_info(cwd=self.INSTALL_PATH)
-        simulation = Exec(f"mpirun -n 3 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-gray-scott settings-files.json", spawn_info)
-        analysis = Exec(f"mpirun -n 2 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-pdf-calc /mnt/nvme/jmendezbenegassimarq/gs.bp /mnt/nvme/jmendezbenegassimarq/pdf.bp 100", spawn_info)
+        simulation = Exec(f"mpirun -n 1 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-gray-scott settings-files.json", spawn_info)
+        analysis = Exec(f"mpirun -n 1 --hostfile {self.HOSTFILE_PATH}/myhosts.txt ./adios2-pdf-calc /mnt/nvme/jmendezbenegassimarq/gs.bp /mnt/nvme/jmendezbenegassimarq/pdf.bp 100", spawn_info)
         self.clean_simulation()
         return simulation.exit_code + analysis.exit_code
