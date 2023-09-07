e67e870b2bc9792383ef122f47dd233056b3fce8
jaime <jcernudagarcia@hawk.iit.edu>
2023-09-07 12:58:19 -0500

    Removing build remote from git

--- build-remote/test/real_apps/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.5TUlmkfNlZ/test/real_apps
-# Build directory: /tmp/tmp.5TUlmkfNlZ/build-remote/test/real_apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gray-scott")
--- build-remote/test/unit/split_single_variable/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/tmp.5TUlmkfNlZ/test/unit/split_single_variable
-# Build directory: /tmp/tmp.5TUlmkfNlZ/build-remote/test/unit/split_single_variable
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
