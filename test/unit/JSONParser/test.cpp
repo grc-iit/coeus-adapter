be8a21014530996782d95afc3a12368193edeb82
jaime <jcernudagarcia@hawk.iit.edu>
2023-09-07 12:14:16 -0500

    Added gtest basic test, Json class and Json tests

---
+++ test/unit/JSONParser/test.cpp
@@ -0,0 +1,23 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Coeus-adapter. The full Coeus-adapter copyright      *
+ * notice, including terms governing use, modification, and redistribution,  *
+ * is contained in the COPYING file, which can be found at the top directory.*
+ * If you do not have access to the file, you may request a copy             *
+ * from scslab@iit.edu.                                                      *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <gtest/gtest.h>
+#include <common/JSONParser.h>
+
+TEST(JSONParser, NoFile) {
+  EXPECT_THROW(JSONParser parser(""), ErrorException);
+}
+
+TEST(JSONParser, ProperFile) {
+  JSONParser parser("../test/unitJSONParser/samples/correct.json");
+  ASSERT_EQ(parser.isValid(), true);
+}
---
+++ test/unit/gtest/gtest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jaime on 8/5/2023.
+//
+#include <gtest/gtest.h>
+#include "MockHermes.h"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
