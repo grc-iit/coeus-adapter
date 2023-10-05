596e27b55a4ebd4263b98b8646141925884699bb
jaime <jcernudagarcia@hawk.iit.edu>
2023-10-05 17:20:04 -0500

    actual data to write

--- test/real_apps/io_comp/io_comp.cpp
+++ test/real_apps/io_comp/io_comp.cpp
@@ -124,13 +124,10 @@ int main(int argc, char *argv[]) {
       adios2::Variable<char> readVariable = io.InquireVariable<char>("data");
 
       auto startGet = std::chrono::high_resolution_clock::now();
-      readEngine.Get<char>(readVariable, data, adios2::Mode::Sync);
+      readEngine.Get<char>(readVariable, data);
+      readEngine.EndStep();
       auto endGet = std::chrono::high_resolution_clock::now();
       localGetTime += std::chrono::duration<double>(endGet - startGet).count();
-
-      readEngine.EndStep();
-
-
       i++;
     }
     readEngine.Close();
