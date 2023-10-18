f1b9913ae70ffecb8a681c1026e7d21332cf48bf
jaime <jcernudagarcia@hawk.iit.edu>
2023-10-18 16:11:17 -0500

    forgot to add the task

---
+++ tasks/coeus_mdm/include/coeus_mdm/coeus_mdm.h
@@ -0,0 +1,83 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef HRUN_coeus_mdm_H_
+#define HRUN_coeus_mdm_H_
+
+#include "coeus_mdm_tasks.h"
+
+namespace hrun::coeus_mdm {
+
+/** Create coeus_mdm requests */
+class Client : public TaskLibClient {
+
+ public:
+  /** Default constructor */
+  Client() = default;
+
+  /** Destructor */
+  ~Client() = default;
+
+  /** Async create a task state */
+  HSHM_ALWAYS_INLINE
+  LPointer<ConstructTask> AsyncCreate(const TaskNode &task_node,
+                                      const DomainId &domain_id,
+                                      const std::string &state_name,
+                                      const std::string &db_path) {
+    id_ = TaskStateId::GetNull();
+    QueueManagerInfo &qm = HRUN_CLIENT->server_config_.queue_manager_;
+    std::vector<PriorityInfo> queue_info = {
+        {1, 1, qm.queue_depth_, 0},
+        {1, 1, qm.queue_depth_, QUEUE_LONG_RUNNING},
+        {qm.max_lanes_, qm.max_lanes_, qm.queue_depth_, QUEUE_LOW_LATENCY}
+    };
+    return HRUN_ADMIN->AsyncCreateTaskState<ConstructTask>(
+        task_node, domain_id, state_name, id_, queue_info, db_path);
+  }
+  HRUN_TASK_NODE_ROOT(AsyncCreate)
+  template<typename ...Args>
+  HSHM_ALWAYS_INLINE
+  void CreateRoot(Args&& ...args) {
+    LPointer<ConstructTask> task =
+        AsyncCreateRoot(std::forward<Args>(args)...);
+    task->Wait();
+    id_ = task->id_;
+    queue_id_ = QueueId(id_);
+    HRUN_CLIENT->DelTask(task);
+  }
+
+  /** Destroy task state + queue */
+  HSHM_ALWAYS_INLINE
+  void DestroyRoot(const DomainId &domain_id) {
+    HRUN_ADMIN->DestroyTaskStateRoot(domain_id, id_);
+  }
+
+  /** Call a custom method */
+  HSHM_ALWAYS_INLINE
+  void AsyncMdm_insertConstruct(Mdm_insertTask *task,
+                            const TaskNode &task_node,
+                            const DomainId &domain_id,
+                            DbOperation db_op) {
+    HRUN_CLIENT->ConstructTask<Mdm_insertTask>(
+        task, task_node, domain_id, id_, db_op);
+  }
+  HSHM_ALWAYS_INLINE
+  void Mdm_insertRoot(const DomainId &domain_id, DbOperation db_op) {
+    LPointer<hrunpq::TypedPushTask<Mdm_insertTask>> task = AsyncMdm_insertRoot(domain_id, db_op);
+//    task.ptr_->Wait();
+  }
+  HRUN_TASK_NODE_PUSH_ROOT(Mdm_insert);
+};
+
+}  // namespace hrun
+
+#endif  // HRUN_coeus_mdm_H_
---
+++ tasks/coeus_mdm/src/coeus_mdm.cc
@@ -0,0 +1,56 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "hrun_admin/hrun_admin.h"
+#include "hrun/api/hrun_runtime.h"
+#include "coeus_mdm/coeus_mdm.h"
+
+#include "common/SQlite.h"
+
+namespace hrun::coeus_mdm {
+
+class Server : public TaskLib {
+private:
+  std::unique_ptr<SQLiteWrapper> db;
+ public:
+  Server() = default;
+
+  void Construct(ConstructTask *task, RunContext &rctx) {
+    db = std::make_unique<SQLiteWrapper>(task->db_path_->str());
+
+    task->SetModuleComplete();
+  }
+
+  void Destruct(DestructTask *task, RunContext &rctx) {
+    task->SetModuleComplete();
+  }
+
+  void Mdm_insert(Mdm_insertTask *task, RunContext &rctx) {
+    DbOperation db_op = task->GetDbOp();
+
+    if (db_op.type == OperationType::InsertData) {
+      db->InsertVariableMetadata(db_op.step, db_op.rank, db_op.metadata);
+      db->InsertBlobLocation(db_op.step, db_op.rank, db_op.name, db_op.blobInfo);
+    } else if (db_op.type == OperationType::UpdateSteps) {
+      db->UpdateTotalSteps(db_op.uid, db_op.currentStep);
+    }
+
+    task->SetModuleComplete();
+  }
+
+ public:
+#include "coeus_mdm/coeus_mdm_lib_exec.h"
+};
+
+}  // namespace hrun::coeus_mdm
+
+HRUN_TASK_CC(hrun::coeus_mdm::Server, "coeus_mdm");
