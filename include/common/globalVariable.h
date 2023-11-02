1030a1848eef0fbf6a1babef142bc846d3f3f73d
linux <hxu40@hawk.iit.edu>
2023-11-02 10:43:35 -0500

    change the output format, temporary remove the global variable

--- include/common/globalVariable.h
+++ include/common/globalVariable.h
@@ -1,3 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Coeus-adapter. The full Coeus-adapter copyright      *
+ * notice, including terms governing use, modification, and redistribution,  *
+ * is contained in the COPYING file, which can be found at the top directory.*
+ * If you do not have access to the file, you may request a copy             *
+ * from scslab@iit.edu.                                                      *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #ifndef COEUS_ADAPTER_GLOBALVARIABLE_H
 #define COEUS_ADAPTER_GLOBALVARIABLE_H
 
