c626e333c5a2ec98cdb30cece1bd30e5d02f083f
jaime <jcernudagarcia@hawk.iit.edu>
2023-11-07 16:04:18 -0600

    return prefetcher wirghts to normal values

--- include/coeus/HermesEngine.h
+++ include/coeus/HermesEngine.h
@@ -154,9 +154,8 @@ class HermesEngine : public adios2::plugin::PluginEngineInterface {
 
   template<typename T>
   DbOperation generateMetadata(adios2::core::Variable<T> variable);
-  template<typename T>
   DbOperation generateMetadata(adios2::core::VariableDerived variable,
-                               T *values, int total_count);
+                               float *values, int total_count);
 
   template<typename T>
   void PutDerived(adios2::core::VariableDerived variable, T *values);
