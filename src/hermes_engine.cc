dc1fd36ac51f7e51bf3876407f297a20fdeb9367
jaime <jcernudagarcia@hawk.iit.edu>
2023-11-06 19:54:38 -0600

    debug derived metadata

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -446,6 +446,7 @@ DbOperation HermesEngine::generateMetadata(adios2::core::VariableDerived variabl
     if (element > max) max = element;
   }
 
+  std::cout << "Bucekt " << Hermes->bkt->name << " blob " << variable.m_Name << " min " << min << " max " << max << " total count " << total_count << std::endl;
   derivedSemantics derived_semantics(min, max);
 
   return DbOperation(currentStep, rank, std::move(vm), variable.m_Name, std::move(blobInfo), derived_semantics);
