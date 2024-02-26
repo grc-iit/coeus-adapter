4342561b194073b21c89cb0a13a02f1142a41ff1
unknown <wardhsu7@gmail.com>
2024-02-26 10:38:35 -0600

    add sin operator

--- test/real_apps/gray-scott/simulation/writer.cpp
+++ test/real_apps/gray-scott/simulation/writer.cpp
@@ -100,11 +100,11 @@ Writer::Writer(const Settings &settings, const GrayScott &sim, adios2::IO io, bo
     if(derived == 1) {
         auto PDFU = io.DefineDerivedVariable("derive/pdfU",
                                              "x:U \n"
-                                             "curl(10, x)",
+                                             "sin(x)",
                                              adios2::DerivedVarType::StoreData);
         auto PDFV = io.DefineDerivedVariable("derive/pdfV",
                                              "x:V \n"
-                                             "curl(10, x)",
+                                             "sin(x)",
                                              adios2::DerivedVarType::StoreData);
     }
 
