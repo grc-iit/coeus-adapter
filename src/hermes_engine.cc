8aafe1e2ed4cd3f0cc6dff2c975bbcd762b34f1b
hxu65 <hxu40@hawk.iit.edu>
2024-06-12 11:27:04 -0500

    testing

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -449,12 +449,12 @@ void HermesEngine::DoPutDeferred_(
   std::string name = variable.m_Name;
   Hermes->bkt->Put(name, variable.SelectionSize() * sizeof(T), values);
   std::cout << getpid()  << "in hermes_engine generate: "  << variable.SelectionSize() * sizeof(T) << std::endl;
-    std::cout << "Shape in hermes_engine PUT: "
+    std::cout << "Shape in hermes_engine PUT: ";
   for (auto i: variable.m_Shape) {
    std::cout << i << " ";
   }
    std::cout << std::endl;
-   std::cout << "Count in hermes_engine PUT: "
+   std::cout << "Count in hermes_engine PUT: ";
     for (auto i: variable.m_Count) {
         std::cout << i << " ";
     }
