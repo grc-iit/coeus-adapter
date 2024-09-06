71cdf9e8694d633a55dff78c6ae980cd402ae14d
hxu65 <hxu40@hawk.iit.edu>
2024-09-05 19:31:47 -0500

    fix function parameters

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -322,10 +322,10 @@ void HermesEngine::ComputeDerivedVariables() {
     if (derivedVar->GetDerivedType() !=
         adios2::DerivedVarType::ExpressionString) {
         std::map<std::string, std::unique_ptr<adios2::MinVarInfo>> NameToMVI;
-        for (auto &pair : nameToVarInfo) {
-            // Move the content of nameToVarInfo into a new unique_ptr in NameToMVI
-            NameToMVI[pair.first] = std::make_unique<adios2::MinVarInfo>(std::move(pair.second));
-        }
+//        for (auto &pair : nameToVarInfo) {
+//
+//            NameToMVI[pair.first] = std::make_unique<adios2::MinVarInfo>(std::move(pair.second));
+//        }
       DerivedBlockData = derivedVar->ApplyExpression(NameToMVI, true);
     }
 
