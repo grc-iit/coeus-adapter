8de8375676589039c2226c1fa3e3acafe4d00c80
hxu65 <hxu40@hawk.iit.edu>
2024-09-27 11:42:52 -0500

    fix issues

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -630,13 +630,13 @@ void HermesEngine::PutDerived(adios2::core::VariableDerived variable,
             Hermes->GetBucket(previous_bucket_name);
             auto blob = Hermes->bkt->Get(name);
             memcpy(values2, blob.data(), blob.size());
-//            for (int i = 0; i < total_count; ++i) {
-//                if (static_cast<int>(values[i]) - static_cast<int>(values2[i]) < 0.01) {
-//                    std::cout << "No difference" << std::endl;
-//                } else {
-//                    std::cout << "difference" << std::endl;
-//                }
-//            }
+            for (int i = 0; i < total_count; ++i) {
+                if (static_cast<int>(values[i]) - static_cast<int>(values2[i]) < 0.01) {
+                    std::cout << "No difference" << std::endl;
+                } else {
+                    std::cout << "difference" << std::endl;
+                }
+            }
         }
     }
 }
