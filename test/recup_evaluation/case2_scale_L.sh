764f97e0f76893e16b09509dbbf26218c801c1d1
hxu65 <hxu40@hawk.iit.edu>
2025-03-21 22:06:05 -0500

    change the script and add explanation for evaluation

--- test/recup_evaluation/case2_scale_L.sh
+++ test/recup_evaluation/case2_scale_L.sh
@@ -17,7 +17,7 @@ for i in ${!nprocs_values[@]}; do
   jarvis pkg config adios2_gray_scott plotgap=1 out_file=/mnt/${location}/hxu40/ofs-mount/case2/${nprocs}process/out2.bp ppn=20 nprocs=${nprocs} checkpoint_output=/mnt/${location}/hxu40/ofs-mount/case2/ckpt.bp steps=$step L=512
   jarvis ppl run &> ~/${report}/${nprocs}process/result1.txt
 
-
+  jarvis cd hashing_compare
   jarvis pkg config hashing_compare ppn=20 nprocs=${nprocs} in_filename=/mnt/${location}/hxu40/ofs-mount/case2/${nprocs}process/out1.bp out_filename=/mnt/${location}/hxu40/ofs-mount/case2/${nprocs}process/out2.bp
   jarvis ppl run &> ~/${report}/${nprocs}process/result2.txt
   rm -r /mnt/${location}/hxu40/ofs-mount/case2/${nprocs}process
