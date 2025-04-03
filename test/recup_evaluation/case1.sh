a139f07e1a5f6dbc615726aa69b30e6a65efa07f
hxu65 <hxu40@hawk.iit.edu>
2025-04-02 21:47:23 -0500

    update deplpy scripts

--- test/recup_evaluation/case1.sh
+++ test/recup_evaluation/case1.sh
@@ -4,8 +4,8 @@
 
 L_values=(134 170 214 268 340 428 540 680 848)
 nprocs_values=(1 2 4 8 16 32 64 128 256)
-location="/add/location"
-report="/add/out_put_file"
+location="/path/to/storage"
+report="/path/to/report/file"
 
 # this is the jarvis pipeline establish, once
 
@@ -15,21 +15,20 @@ for i in ${!L_values[@]}; do
   jarvis cd gray_scott_bp5
   # First configuration and run
   mkdir -p ~/${report}/${nprocs}process
-  jarvis pkg config adios2_gray_scott plotgap=1 out_file=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs steps=40 L=$L checkpoint_output=/mnt/${location}/hxu40/ofs-mount/case1/ckpt.bp
-  jarvis pkg config adios_hashing in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy1.bp
-  jarvis ppl run >> ~/${report}/${nprocs}process/result1.txt
+  jarvis pkg config adios2_gray_scott plotgap=1 out_file=${location}/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs steps=40 L=$L checkpoint_output=${location}/${nprocs}process/ckpt.bp
+  jarvis pkg config adios_hashing in_filename=${location}/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs out_filename=${location}/${nprocs}process/copy1.bp
+  jarvis ppl run >> ${report}/${nprocs}process/result1.txt
 
 
   # Second configuration and run
   jarvis pkg config adios2_gray_scott plotgap=1 out_file=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs steps=40 L=$L checkpoint_output=/mnt/${location}/hxu40/ofs-mount/case1/ckpt.bp
   jarvis pkg config adios_hashing in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy2.bp
-  jarvis ppl run >> ~/${report}/${nprocs}process/result2.txt
+  jarvis ppl run >> ${report}/${nprocs}process/result2.txt
 
 
   # Hashing compare
-  jarvis cd hashing_compare
-  jarvis pkg config hashing_compare nprocs=$nprocs ppn=20 in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy1.bp out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy2.bp
-  jarvis ppl run >> ~/${report}/${nprocs}process/result3.txt
-  rm -r /mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process
+  jarvis cd hashing_compare${location}/${nprocs}process/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy2.bp
+  jarvis ppl run >> ${report}/${nprocs}process/result3.txt
+  rm -r ${location}/${nprocs}process
 
 done
--- test/recup_evaluation/case1_scale_L.sh
+++ test/recup_evaluation/case1_scale_L.sh
@@ -3,29 +3,29 @@
 # Define variables
 steps=(2 4 8 16 32 64 128 256 512)
 nprocs_values=(1 2 4 8 16 32 64 128 256)
-location="nvme"
-report="case1_fix_L_nvme_1"
+location="/path/to/storage"
+report="/path/to/report/file"
 for i in ${!steps[@]}; do
   step=${steps[$i]}
   nprocs=${nprocs_values[$i]}
   jarvis cd gray_scott_bp5
   # First configuration and run
-  mkdir -p ~/${report}/${nprocs}process
-  jarvis pkg config adios2_gray_scott plotgap=1 out_file=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs steps=$step L=512
-  jarvis pkg config adios_hashing in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy1.bp
-  jarvis ppl run > ~/${report}/${nprocs}process/result1.txt
-  rm -r /mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out1.bp
+  mkdir -p ${report}/${nprocs}process
+  jarvis pkg config adios2_gray_scott plotgap=1 out_file=${location}/${nprocs}process/out1.bp ppn=20 nprocs=$nprocs steps=$step L=512
+  jarvis pkg config adios_hashing in_filename=${location}/out1.bp ppn=20 nprocs=$nprocs out_filename=${location}/${nprocs}process/copy1.bp
+  jarvis ppl run > ~/${report}/${nprocs}process/gray_scott_1.txt
+
 
   # Second configuration and run
-  jarvis pkg config adios2_gray_scott plotgap=1 out_file=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs steps=$step L=512
-  jarvis pkg config adios_hashing in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy2.bp
-  jarvis ppl run > ~/${report}/${nprocs}process/result2.txt
-  rm -r /mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/out2.bp
+  jarvis pkg config adios2_gray_scott plotgap=1 out_file=${location}/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs steps=$step L=512
+  jarvis pkg config adios_hashing in_filename=${location}/${nprocs}process/out2.bp ppn=20 nprocs=$nprocs out_filename=${location}/${nprocs}process/copy2.bp
+  jarvis ppl run > ~/${report}/${nprocs}process/gray_scott_2.txt
+
 
   # Hashing compare
   jarvis cd hashing_compare
-  jarvis pkg config hashing_compare ppn=20 nprocs=$nprocs in_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy1.bp out_filename=/mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process/copy2.bp
-  jarvis ppl run > ~/${report}/${nprocs}process/result3.txt
-  rm -r /mnt/${location}/hxu40/ofs-mount/case1/${nprocs}process
+  jarvis pkg config hashing_compare ppn=20 nprocs=$nprocs in_filename=${location}/${nprocs}process/copy1.bp out_filename=${location}/${nprocs}process/copy2.bp
+  jarvis ppl run > ${report}/${nprocs}process/compare.txt
+  rm -r ${location}/${nprocs}process
 
 done
