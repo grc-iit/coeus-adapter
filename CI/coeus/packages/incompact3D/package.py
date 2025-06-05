892cb914782750d8955e323b5b95746c70831c8b
hxu65 <hxu40@hawk.iit.edu>
2025-06-05 09:48:32 -0500

    add patch() function to replace the place

--- CI/coeus/packages/incompact3D/package.py
+++ CI/coeus/packages/incompact3D/package.py
@@ -27,20 +27,20 @@ class Incompact3d(CMakePackage):
 
     # Write the modified content back
 
-    def patch(self):
-        """Dynamically replace hardcoded path in the patch file before applying."""
-        patch_file = os.path.join(self.package_dir, 'add_path_command.patch')
-
-        # Define the old and new path
-        old_path = '/path/to/decomp_fix.patch'
-        new_path = self.package_dir + '/patches/decomp_fix.patch'  # Ensure trailing slash
-
-        # Read and replace content
-        with open(patch_file, 'r') as f:
-            content = f.read()
-        content = content.replace(old_path, new_path)
-        with open(patch_file, 'w') as f:
-            f.write(content)
+    # def patch(self):
+    #     """Dynamically replace hardcoded path in the patch file before applying."""
+    #     patch_file = os.path.join(self.package_dir, 'add_path_command.patch')
+    #
+    #     # Define the old and new path
+    #     old_path = '/path/to/decomp_fix.patch'
+    #     new_path = self.package_dir + '/patches/decomp_fix.patch'  # Ensure trailing slash
+    #
+    #     # Read and replace content
+    #     with open(patch_file, 'r') as f:
+    #         content = f.read()
+    #     content = content.replace(old_path, new_path)
+    #     with open(patch_file, 'w') as f:
+    #         f.write(content)
 
 
     patch('add_path_command.patch', when='@coeus')
