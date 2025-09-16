644458274910f912b3103442424a52e1d2757d60
ubuntu <huax08@gmail.com>
2025-09-16 08:52:10 -0500

    make it general add derived variables

--- test/real_apps/gray-scott/simulation/writer.cpp
+++ test/real_apps/gray-scott/simulation/writer.cpp
@@ -98,14 +98,14 @@ Writer::Writer(const Settings &settings, const GrayScott &sim, adios2::IO io, bo
 
     if(derived == 1) {
         std::cout << "use derived variables" << std::endl;
-        auto PDFU = io.DefineDerivedVariable("derive/hashU",
+        auto PDFU = io.DefineDerivedVariable("derive/AddU",
                                              "x = U \n"
-                                             "hash(x)",
+                                             "add(x)",
                                              adios2::DerivedVarType::StoreData);
 
-        auto PDFV = io.DefineDerivedVariable("derive/hashV",
+        auto PDFV = io.DefineDerivedVariable("derive/AddV",
                                              "x = V \n"
-                                             "hash(x)",
+                                             "add(x)",
                                              adios2::DerivedVarType::StoreData);
 
     }
