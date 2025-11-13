edb7d07ff2314b34be4aaafdb4d2a60349a60efe
hxu65 <hxu40@hawk.iit.edu>
2025-11-13 11:25:21 -0600

    scipts modification

--- src/hermes_engine.cc
+++ src/hermes_engine.cc
@@ -31,7 +31,7 @@ HermesEngine::HermesEngine(adios2::core::IO &io,//NOLINT
   Hermes = std::make_shared<coeus::Hermes>();
   //  mpiComm = std::make_shared<coeus::MPI>(comm.Duplicate());
   Init_();
-  engine_logger->info("rank {} with name {} and mode {}", rank, name, adios2::ToString(mode));
+  //engine_logger->info("rank {} with name {} and mode {}", rank, name, adios2::ToString(mode));
 
 
 }
@@ -46,7 +46,7 @@ HermesEngine::HermesEngine(std::shared_ptr<coeus::IHermes> h,
     : adios2::plugin::PluginEngineInterface(io, name, mode, comm.Duplicate()) {
   Hermes = h;
   Init_();
-  engine_logger->info("rank {} with name {} and mode {}", rank, name, adios2::ToString(mode));
+  //engine_logger->info("rank {} with name {} and mode {}", rank, name, adios2::ToString(mode));
 
 }
 
